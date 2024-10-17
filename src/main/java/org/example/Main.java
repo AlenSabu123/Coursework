8c083a15ae6de57274e265fe0180275ae28c2e7f
AlenSabu123 <40683609@live.napier.ac.uk>
2024-10-17 14:55:57 +0100

    added world.sql to datatbase folder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Main {
+    /**
+     * jijijoi
+     * @param args ioilujlouj
+     */
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
