807541494a0da717ff5d8053450b3520f04a68f8
AlenSabu123 <40683609@live.napier.ac.uk>
2024-11-14 12:45:24 +0000

    added comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,14 @@ public class Main {
      * jijijoi
      * @param args ioilujlouj
      */
+    //main method
     public static void main(String[] args) {
 
+        //intialisation of app object.
         App a = new App();
+        //connect to database
         a.connect();
+        //disconnect from database.
         a.disconnect();
 
 
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,12 +1,14 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
+//class for test
 class MyTest
 {
+    //method for unit test.
     @Test
     void unitTest()
     {
+        //check if the two values are equal.
         assertEquals(5, 5);
     }
 }
